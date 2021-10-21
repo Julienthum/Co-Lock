3ec7928501289c443555eed855a3a0d46a5c68a3
Kelyan123 <j.mpiryaminata@students.ephec.be>
2021-10-21 16:04:43 +0200

    liste dynamique biens

---
+++ testv/src/app/mesbiens/mesbiens.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mesbiens',
+  templateUrl: './mesbiens.page.html',
+  styleUrls: ['./mesbiens.page.scss'],
+})
+export class MesbiensPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
