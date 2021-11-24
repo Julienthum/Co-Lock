08da32c28085a379bc567b366dc3f48e63172baf
Alban Van Beirs <a.vanbeirs@students.ephec.be>
2021-11-24 19:14:05 +0100

    Liaison users-biens
    Liaison des collections grâce a l'ID de l'utilisateur et affichage uniquement des biens enregistré par l'utilisateur

--- Co-Lock/src/app/ajoutbien/ajoutbien.page.ts
+++ Co-Lock/src/app/ajoutbien/ajoutbien.page.ts
@@ -3,6 +3,12 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { AlertController } from '@ionic/angular/';
 
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/auth';
+
 @Component({
   selector: 'app-ajoutbien',
   templateUrl: './ajoutbien.page.html',
@@ -24,11 +30,14 @@ export class AjoutbienPage implements OnInit {
     prix: '',
     image: ''
   };
+  essaieForm: any;
+
 
   constructor(
     public firestore: AngularFirestore,
     // eslint-disable-next-line @typescript-eslint/no-shadow
     public alertController: AlertController,
+    public formBuilder: FormBuilder,
   ) {
     this.users = this.firestore.collection('biens').valueChanges();
   }
@@ -38,6 +47,19 @@ export class AjoutbienPage implements OnInit {
   };
 
   ngOnInit() {
+    this.essaieForm = this.formBuilder.group({
+      nomDuBien: '',
+      description: '',
+      rue: '',
+      numero: '',
+      ville: '',
+      codepostal: '',
+      province: '',
+      superficie: '',
+      nbrePlace: '',
+      prix: '',
+      image: ''
+      });
   }
 
   async presentAlert() {
@@ -50,5 +72,37 @@ export class AjoutbienPage implements OnInit {
 
     await alert.present();
   };
+
+  async addMike(){ // ca ajoute un bien dans la collec biens et ca ajoute aussi l'uid dans les champs
+    const user = await firebase.auth().currentUser;
+    const moi = user.uid;
+    const name = this.essaieForm.value.nomDuBien;
+    const description = this.essaieForm.value.description;
+    const rue = this.essaieForm.value.rue;
+    const numero = this.essaieForm.value.numero;
+    const ville = this.essaieForm.value.ville;
+    const cp = this.essaieForm.value.codepostal;
+    const province = this.essaieForm.value.province;
+    const superficie = this.essaieForm.value.superficie;
+    const nbrePlace = this.essaieForm.value.nbrePlace;
+    const prix = this.essaieForm.value.prix;
+    const image = this.essaieForm.value.image;
+    this.firestore.collection('biens').add({
+      moi,
+      name,
+      description,
+      rue,
+      numero,
+      ville,
+      cp,
+      province,
+      superficie,
+      nbrePlace,
+      prix,
+      image,
+    });
+
+    console.log('ca marche');
+  }
 
 }
--- Co-Lock/src/app/mesbiens/mesbiens.page.ts
+++ Co-Lock/src/app/mesbiens/mesbiens.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { user } from '@angular/fire/auth';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
+
+import firebase from 'firebase/compat/app';
+import 'firebase/auth';
 
 @Component({
   selector: 'app-mesbiens',
@@ -9,15 +13,25 @@ import { Observable } from 'rxjs';
 })
 export class MesbiensPage implements OnInit {
   biens: Observable<any[]>;
+  query: true;
 
   constructor(
     public firestore: AngularFirestore,
   ) {
-    this.biens = this.firestore.collection('biens').valueChanges();
+
   }
 
 
   ngOnInit() {
+    const id = firebase.auth().currentUser.uid;
+    const biens = this.firestore.collection('biens');
+    const query = biens.ref.where('moi', '==', id);
+    if(query){
+      console.log(id);
+      this.biens = this.firestore.collection('biens',  (ref) =>
+        ref.where('moi', '==', id)).valueChanges();
+    } else {
+      console.log('ca marche bien ');
   }
-
+  }
 }
